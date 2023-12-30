fd0b913bba885609fccf2855d5b8b0d31fa96d72
unknown <rpranay14@gmail.com>
2023-12-30 20:33:10 +0530

    loading spinner on submit button

--- app/issues/new/page.js
+++ app/issues/new/page.js
@@ -1,35 +1,49 @@
 'use client'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextArea, TextField } from '@radix-ui/themes'
 import React, { useState } from 'react';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import Spinner from '@/app/Components/Spinner';
 
 const NewIssue = () => {
     const [title, setTitle] = useState("");
     const [description,setDescription]=useState('');
+    const [error,setError]=useState(null)
     const router=useRouter()
+    const [isSubmitting,setIsSubmitting]=useState(false)
+
     const handleSubmit=async()=>{
         console.log(title,description);
         const issue={title,description};
-        const response=await axios.post('http://localhost:3001/issue',issue)
-        if(response.data.success){
-          console.log("newws")
-          router.push('/issues')
-        }
-        console.log(response.data)
+        try {
+          setIsSubmitting(true)
+          const response=await axios.post('http://localhost:3001/issue',issue)
+          if(response.data.success){
+            console.log("newws")
+            router.push('/issues')
+          }
+        } catch (error) {
+          setError("Something went wrong")
+        }   
+        finally{
+          setIsSubmitting(false)
+        } 
     }
     const onChange = (value) => {
        setDescription(value)
       }
   return (
     <div className='p-8 w-[30rem] space-y-2'>
+     {error ? <Callout.Root color='red'>
+        <Callout.Text>{error}</Callout.Text>
+      </Callout.Root> :null}
         <TextField.Root>
             <TextField.Input placeholder='Title' value={title} onChange={(e)=>setTitle(e.target.value)}></TextField.Input>
         </TextField.Root>
        <SimpleMDE  value={description} onChange={onChange}/>
-        <Button onClick={()=>handleSubmit()}>Submit New Issue</Button>
+        <Button disabled={isSubmitting} onClick={()=>handleSubmit()}>Submit New Issue {isSubmitting ?<Spinner/> :null}</Button>
     </div>
   )
 }
