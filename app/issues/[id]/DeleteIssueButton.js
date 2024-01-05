ba34b8d6369a2ad0b0c675579ebeb7db8c7a6bcc
Pranay Rawat <121486679+rpranay14@users.noreply.github.com>
2024-01-05 21:21:41 +0530

    delete issue

--- app/issues/[id]/DeleteIssueButton.js
+++ app/issues/[id]/DeleteIssueButton.js
@@ -1,8 +1,20 @@
 'use client'
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
-const DeleteIssueButton = () => {
+
+const DeleteIssueButton = ({issueId}) => {
+const router=useRouter()
+    const handleDelete=async()=>{
+        const response=await axios.delete(`http://localhost:3001/issue/${issueId}`);
+        if(response.data.success){
+            router.push('/issues');
+            router.refresh();
+        }
+
+    }
   return (
    <AlertDialog.Root>
     <AlertDialog.Trigger>
@@ -13,7 +25,7 @@ const DeleteIssueButton = () => {
         <AlertDialog.Description>Are you sure you want to delete? This action cannot be undone</AlertDialog.Description>
     <Flex gap="2" className='mt-4'>
     <AlertDialog.Cancel><Button variant="soft" color='gray'>Cancel</Button></AlertDialog.Cancel>
-    <AlertDialog.Cancel><Button variant="soft" color='red'>Delete</Button></AlertDialog.Cancel>
+    <AlertDialog.Cancel><Button variant="soft" color='red' onClick={()=>handleDelete()}>Delete</Button></AlertDialog.Cancel>
     </Flex>
     </AlertDialog.Content>
    </AlertDialog.Root>
